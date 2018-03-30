f37c79fa827c8dfdfcf7cb52f3c4764fa80d0a9f
Margaret Kosmala <mkosmala@gmail.com>
2018-03-29 22:13:59 -0400

    Comments

--- SimSimbaDiseaseModel/Engine.h
+++ SimSimbaDiseaseModel/Engine.h
@@ -9,6 +9,16 @@ namespace SimSimba {
 
 	class Engine {
 	public:
+		/**
+		Constructor
+		@param pname name of the parameter file
+		@param fname name of the input population file
+		@param steps number of steps (half-years) to run each simulation
+		@param outpop name of the output population file
+		@param fstat name of the output statistics file
+		@param repl number of replicates (simulations) to run
+		@param trans whether or not to create a transcript of the simulation (warning: large!)
+		*/
 		Engine(string pname,string fname,int steps,string outpop,string fstat,int repl,bool trans) { 
 			paramname = pname;
 			filename = fname; 
